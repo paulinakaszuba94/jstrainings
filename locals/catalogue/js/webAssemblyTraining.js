99e4033f167f6bebafbdc3a0e04ab8f24294de22
webdwarves <contact@webdwarves.com>
2019-09-15 21:45:07 +0200

    lexical fixes

--- locals/catalogue/js/webAssemblyTraining.js
+++ locals/catalogue/js/webAssemblyTraining.js
@@ -4,7 +4,7 @@ var webassembly_1 = require("../../trainings/webassembly");
 exports.webAssemblyTraining = {
     name: 'Wykorzystywanie kodu Java, Rust, C++' +
         ' w przeglądarce - wprowadzenie do WebAssembly',
-    description: webassembly_1.webassembly.mainDescription + "\n" + webassembly_1.webassembly.detailDescription,
+    description: webassembly_1.webassembly.mainDescription,
     duration: 2,
     profile: 'Szkolenie adresowane jest dla osób, które znają podstway języka JavaScript oraz dowolngo innego języka' +
         ' programowania',
--- locals/catalogue/js/webAssemblyTraining.ts
+++ locals/catalogue/js/webAssemblyTraining.ts
@@ -3,7 +3,7 @@ import {webassembly} from "../../trainings/webassembly";
 export const webAssemblyTraining = {
     name: 'Wykorzystywanie kodu Java, Rust, C++' +
         ' w przeglądarce - wprowadzenie do WebAssembly',
-    description: webassembly.mainDescription + "\n" + webassembly.detailDescription,
+    description: webassembly.mainDescription,
     duration: 2,
     profile: 'Szkolenie adresowane jest dla osób, które znają podstway języka JavaScript oraz dowolngo innego języka' +
         ' programowania',
