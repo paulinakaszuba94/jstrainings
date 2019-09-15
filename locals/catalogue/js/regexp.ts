ef61acbab275402d15cfab1f3f81100b67c75b72
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 23:44:57 +0200

    Update regexp.ts

--- locals/catalogue/js/regexp.ts
+++ locals/catalogue/js/regexp.ts
@@ -1,7 +1,7 @@
 export const regexTraining = {
     name: `regExp - wyrażenia regularne, zostań mistrzem!`,
     description: `
-Celem szkolenia jest nauczenie uczestników, w jaki sposób osiągnąć mistrzostwo w tworzeniu wyrażeń regularnych. Szkolenie jest bogate w ćwiczenia, dzięki czemu każdy uczestnik będzie w stanie utrwalić nabytą wiedzę`,
+Celem szkolenia jest nauczenie uczestników, w jaki sposób osiągnąć mistrzostwo w tworzeniu wyrażeń regularnych. Szkolenie jest bogate w ćwiczenia, dzięki czemu każdy uczestnik będzie w stanie utrwalić nabytą wiedzę.`,
 
     profile: `
 Szkolenie adresowane jest do programistów w dowolnych językach programownia.`,
