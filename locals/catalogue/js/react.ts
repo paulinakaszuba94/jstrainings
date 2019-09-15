2ed5b14f3b68e33d33c9ba9e71b3a75e3106d5d6
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 22:55:13 +0200

    Update react.ts

--- locals/catalogue/js/react.ts
+++ locals/catalogue/js/react.ts
@@ -3,7 +3,7 @@ import {react} from "../../trainings/react";
 export const reactTraining = {
     name: `React.js`,
     description: `${react.detailDescription} 
-Celem szkolenia jest nauczenie od podstaw biblioteki React.js oraz praktycznego jej wykorzystania do tworzenia nowoczesnych aplikacji internetowych. Uczestnicy dowiedzą się w jaki sposób dzielić aplikacje na komponenty oraz na czym polegają współczesne architektury do zarządzania stanem aplikacji - takie jak React hooks, Redux czy MobX i czym różnią się od klasycznych podejść  typu Model-View-Controller.
+Celem szkolenia jest nauczenie od podstaw biblioteki React.js oraz praktycznego jej wykorzystania do tworzenia nowoczesnych aplikacji internetowych. Uczestnicy dowiedzą się, w jaki sposób dzielić aplikacje na komponenty oraz na czym polegają współczesne architektury do zarządzania stanem aplikacji - takie jak React hooks, Redux czy MobX i czym różnią się od klasycznych podejść typu Model-View-Controller.
 Po ukończonym szkoleniu uczestnicy będą w stanie samodzielnie pisać aplikacje wykorzystujące bibliotekę React. `,
 
 
@@ -14,7 +14,7 @@ Szkolenie adresowane jest do programistów znających język JavaScript, chcący
     list: react.list,
 
     skills: `
-Po ukończonym szkoleniu uczestnicy będą znać zasady działania biblioteki React i będą wiedzieć jak użyć tej biblioteki razem z architekturami Fluxowymi takimi jak Redux oraz MobX. oraz z React hooks Zdobędą informacje na temat dobrych praktyk, podejścia funkcyjnego oraz tworzenia przejrzystych struktur aplikacji reactowych.
+Po ukończonym szkoleniu uczestnicy będą znać zasady działania biblioteki React i będą wiedzieć, jak użyć tej biblioteki razem z menadżerami stanów, takimi jak Redux, MobX czy React hooks. Ponadto zdobędą informacje na temat dobrych praktyk, podejścia funkcyjnego oraz tworzenia przejrzystych struktur aplikacji reactowych.
 `
 
 
