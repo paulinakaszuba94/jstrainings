02b13d0c1eeadfb3f228b7325a9b814c50f14a1c
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 23:04:01 +0200

    Update testyZaawansowane.ts

--- locals/catalogue/js/testyZaawansowane.ts
+++ locals/catalogue/js/testyZaawansowane.ts
@@ -1,26 +1,26 @@
 export const testsAdvancedTraining = {
     name: `Testy automatyczne aplikacji webowych - poziom zaawansowany`,
     description: `
-Celem szkolenia jest poszerzenie wiadomości uczestników na temat testowania aplikacji webowych. Uczestnicy poznają bardziej zaawansowane techniki testowania jak i bardziej specjalistyczne narzędzia do tworzenia testów.`,
+Celem szkolenia jest poszerzenie wiadomości uczestników na temat testowania aplikacji webowych. Uczestnicy poznają bardziej zaawansowane techniki testowania, jak i bardziej specjalistyczne narzędzia do tworzenia testów.`,
     profile: `
 Szkolenie adresowane jest do programistów, którzy posiadają wiadomości ze szkolenia Testy Automatyczne dla programistów.`,
     duration:
         1,
     list: [
-        `przypomnienie jasmine`,
+        `Przypomnienie jasmine`,
         `jasmine zaawansowany - custom matchers, custom equality testers`,
-        `generatory asercji`,
-        `property based testing`,
+        `Generatory asercji`,
+        `Property based testing`,
         `sinon.js`,
-        `gherkin w szczegółach`,
-        `puppeteer`,
+        `Gherkin w szczegółach`,
+        `Puppeteer`,
         `CSS testing - visual regression testing tools`,
         `HTML testing`,
-        `karma`],
+        `Karma`],
 
 
     skills: `
-Po ukończonym szkoleniu uczestnicy będą posiadać wiedzę na temat tworzenia zaawansowanych testów
+Po ukończonym szkoleniu uczestnicy będą posiadać wiedzę na temat tworzenia zaawansowanych testów.
 
 `
 }
