7b976b7406da04340a07b530cebc5143e7e1e515
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 22:32:33 +0200

    Update wstepDoProgramowania.ts

--- locals/catalogue/js/wstepDoProgramowania.ts
+++ locals/catalogue/js/wstepDoProgramowania.ts
@@ -1,6 +1,6 @@
 export const introductionToProgrammingTraining = {
     name: 'Wstęp do programowania z JavaScript',
-    description: 'Celem szkolenia jest nauczenie uczestników podstaw programowania w języku JavaScript. Jest to kurs dla osób, które dopiero chcą zacząć swoją przygodę z programowaniem. Uczestnicy poznają podstawy programowania takie jak instrukcje warunkowe, zmienne czy pętle, zrozumieją na czym polegają różne podejścia programistyczne i dostaną szereg narzędzi pozwalających na późniejszy samodzielny rozwój.',
+    description: 'Celem szkolenia jest nauczenie uczestników podstaw programowania w języku JavaScript. Jest to kurs dla osób, które dopiero chcą zacząć swoją przygodę z programowaniem. Uczestnicy poznają podstawy programowania takie jak instrukcje warunkowe, zmienne czy pętle, zrozumieją, na czym polegają różne podejścia programistyczne i dostaną szereg narzędzi pozwalających na późniejszy samodzielny rozwój.',
     duration: 3,
     profile: 'Szkolenie adresowane jest dla osób, które nie potrafią jeszcze programować w żadnym języku.',
     list: [
@@ -13,7 +13,7 @@ export const introductionToProgrammingTraining = {
         "Czym są obiekty i jak ich używać",
         "Jak używać JavaScript do interakcji ze stroną internetową",
         "Komunikacja z serwerem",
-        "Skąd czerpać wiedzę i jak się dalej  rozwijać",
+        "Skąd czerpać wiedzę i jak rozwijać się dalej",
     ]
 
 }
