ce99ef2f1154d2ed3ff45051e76401bbb439ee6b
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 21:07:48 +0200

    Update pwa.ts

--- locals/trainings/pwa.ts
+++ locals/trainings/pwa.ts
@@ -1,12 +1,12 @@
 export const pwa = {
     "id": "pwa",
     "list": ["Mechanika nowoczesnych systemów JavaScriptowych", "WebWorkers w szczegółach", "Service Worker", "Progressive enhancement", "Manifest JSON", "Nieblokujące operacje", "Integracja z systemem operacyjnym"],
-    "detailDescription": "Celem szkolenia jest przekazać wiedzę na temat technologii związanych z progressive web" +
-        " applications. Uczestnicy dowiedzą się w jaki sposób budować skalowalne aplikacje, które z poziomu" +
+    "detailDescription": "Celem szkolenia jest przekazanie wiedzy na temat technologii związanych z progressive web" +
+        " applications. Uczestnicy dowiedzą się, w jaki sposób budować skalowalne aplikacje, które z poziomu" +
         " użytkownika będą wyglądały jak aplikacje natywne",
     "name": "Web Workers i Progressive Web Apps",
     "en": {},
-    "mainDescription": " W dzisiejszym świecie za pomocą JavaScriptu można stworzyć aplikacje webowe, które mogą być uruchomione jak aplikacja natywna na telefonie czy desktopie. Odpowiednio zbudowana aplikacja będzie posiadała swoją ikonę, może działać offline dzięki czemu użytkownik korzysta z niej jak z każdej innej aplikacji. \n ",
+    "mainDescription": " W dzisiejszym świecie za pomocą JavaScriptu można stworzyć aplikacje webowe, które mogą być uruchomione jak aplikacja natywna na telefonie czy desktopie. Odpowiednio zbudowana aplikacja będzie posiadała swoją ikonę, może działać offline, dzięki czemu użytkownik korzysta z niej tak, jak z każdej innej aplikacji. \n ",
     "alt": "Web Workers i Progressive Web Apps",
     "img": "./img/portfolio/thumbnails/pwa.png",
     "src": ".././img/portfolio/thumbnails/pwa.png",
