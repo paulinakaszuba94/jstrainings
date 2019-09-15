eb0b9183c5e58168a048e938a22cc5ea8fb0a272
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 22:00:11 +0200

    Update 2020 - rok JavaScriptu (Angular, react, vue).ts

--- locals/catalogue/js/2020 - rok JavaScriptu (Angular, react, vue).ts
+++ locals/catalogue/js/2020 - rok JavaScriptu (Angular, react, vue).ts
@@ -4,7 +4,7 @@ import {angularAdvancedTraining} from "./angularZaawansowany";
 export const year2020 = {
     name: `2020 - rok JavaScriptu (Angular, react, vue)`,
     description:`
-W czasie szkolenia uczestnicy będą mieli szansę dowiedzieć się jak działąją obecnie najpopularniejsze biblitoeki/frameworki na rynku aplikacji front-endowych. Skupimy się na ich wadach, zaletach, podobieństwach i różnicach tak aby uczestnicy mogli podejmować świadome decyzje dotyczące wyboru technologii do projektu.`,
+W czasie szkolenia uczestnicy będą mieli szansę dowiedzieć się, jak działąją obecnie najpopularniejsze biblioteki/frameworki na rynku aplikacji frontendowych. Skupimy się na ich wadach, zaletach, podobieństwach i różnicach tak, aby uczestnicy mogli podejmować świadome decyzje dotyczące wyboru technologii do projektu.`,
     profile: `
 Szkolenie adresowane jest do programistów znających język JavaScript`,
     duration:
