c15f12e4c87858cd90fe69554dde7a4b7c8b8062
Paulina <55359571+paulinakaszuba94@users.noreply.github.com>
2019-09-15 22:15:46 +0200

    Update es6.ts

--- locals/catalogue/js/es6.ts
+++ locals/catalogue/js/es6.ts
@@ -3,11 +3,11 @@ import {javascript} from "../../trainings/javascript";
 export const es6Training = {
     name: `ES6+`,
     description: `
-Celem szkolenia jest przekazanie wiedzy na temat nowych featurów języka JavaScript, które zostały zaimplementowane w standardach EcmaScript nowszych niż EcmaScript 5. Uczestnicy nauczą się podstawowych mechanizmów takich jak zmienne blokowe jak również tych bardziej skomplikowanych takich jak iteratory, generatory czy metaprogrmowanie przy użyciu proxy
+Celem szkolenia jest przekazanie wiedzy na temat nowych featurów języka JavaScript, które zostały zaimplementowane w standardach EcmaScript, nowszych niż EcmaScript 5. Uczestnicy nauczą się podstawowych mechanizmów, takich jak zmienne blokowe, jak również tych bardziej skomplikowanych, jak iteratory, generatory czy metaprogrmowanie przy użyciu proxy
 `,
 
     profile: `
-Szkolenie adresowane jest do programistów znających język JavaScript w wersji EcmaScript 5 chcących poznać możliwości języka, które zostały dodane w standardach EcmaScript 2015 i kolejnych (czyli ES6+ ).`,
+Szkolenie adresowane jest do programistów znających język JavaScript w wersji EcmaScript 5, chcących poznać możliwości języka, które zostały dodane w standardach EcmaScript 2015 i kolejnych (czyli ES6+ ).`,
 
     duration:
         1,
